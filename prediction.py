fdbaa56d070638607f6a6840f102c21c57a92732
Shashank Harinath <Shank@Shashanks-MacBook-Pro.local>
2016-01-15 02:23:03 +0530

    Cross validation

--- prediction.py
+++ prediction.py
@@ -1,15 +1,11 @@
 from sklearn.linear_model import SGDClassifier
 from sklearn.pipeline import Pipeline
-from sklearn import metrics
-from sklearn import svm
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn import cross_validation
 import pickle
 import numpy as np
-
-text_clf = Pipeline([('vect', CountVectorizer()),
-	('tfidf', TfidfTransformer()),
-	('clf', SGDClassifier(loss='hinge', penalty='l2', alpha=1e-4, n_iter=20, verbose=1, random_state=57)),])
+from numpy import array
 
 pos = open('pos.pkl', 'r')
 neg = open('neg.pkl', 'r')
@@ -28,13 +24,40 @@ test = []
 test.extend(neg_list[8000:])
 test.extend(pos_list[2953:])
 
+
 tr_labels = [0] * len(neg_list[1:8000])
 tr_labels.extend([1] * len(pos_list[1:2953]))
 
 te_labels = [0] * len(neg_list[8000:])
 te_labels.extend([1] * len(pos_list[2953:]))
 
-text_clf.fit(train, tr_labels)
+text_clf = Pipeline([('vect', CountVectorizer()),
+	('tfidf', TfidfTransformer()),
+	('clf', SGDClassifier(loss='hinge', penalty='l2', alpha=1e-4, n_iter=20, verbose=1, random_state=57)),])
 
+text_clf.fit(train, tr_labels)
 predicted = text_clf.predict(test)
+print("80-20 split:")
 print(np.mean(predicted == te_labels))
+
+X = []
+X.extend(neg_list)
+X.extend(pos_list)
+
+X = array(X)
+
+y = [0] * len(neg_list)
+y.extend([1] * len(pos_list))
+
+y = array(y)
+
+cv = cross_validation.KFold(len(X), n_folds=5)
+res = []
+
+for tr, te in cv:
+	text_clf.fit(X[tr], y[tr])
+	predicted = text_clf.predict(X[te])
+	res.append(np.mean(predicted == y[te]))
+
+print("5-fold cross validation:")
+print(res)
