58b3c592dc00f0177f657dff7e12dd6c9393b371
Shashank Harinath <Shank@Shashanks-MacBook-Pro.local>
2016-01-15 09:35:26 +0530

    Driver class

---
+++ driver.py
@@ -0,0 +1,11 @@
+from parser import Parser
+from prediction import Predict
+
+def main():
+	# Parser.parse()
+	predict = Predict()
+	#predict.classfication_split()
+	predict.classification_cv(5)
+
+if __name__ == "__main__":
+	main()
